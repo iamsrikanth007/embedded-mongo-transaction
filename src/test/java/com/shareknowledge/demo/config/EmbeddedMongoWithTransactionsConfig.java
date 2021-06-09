156a160003f674c7524bcda1df58a6fd040208fa
srikanth.p <srikanth.p@techsophy.com>
2021-06-09 19:43:28 +0530

    fix

---
+++ src/test/java/com/shareknowledge/demo/config/EmbeddedMongoWithTransactionsConfig.java
@@ -0,0 +1,124 @@
+package com.shareknowledge.demo.config;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongoCmdOptions;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import org.bson.Document;
+import org.springframework.boot.autoconfigure.AbstractDependsOnBeanFactoryPostProcessor;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Class for auto-configuring and starting an embedded MongoDB with support for transactions.
+ * As there's some overhead in using it and slower startup time, use it only if support for
+ * transactions is needed.
+ */
+@Profile("test")
+@Configuration(proxyBeanMethods = false)
+@AutoConfigureBefore({MongoAutoConfiguration.class})
+@ConditionalOnClass({MongoClient.class, MongodStarter.class})
+@Import({
+        EmbeddedMongoAutoConfiguration.class,
+        EmbeddedMongoWithTransactionsConfig.DependenciesConfiguration.class
+})
+public class EmbeddedMongoWithTransactionsConfig {
+
+    // You may get a warning in the log upon shutdown like this:
+    // "...Destroy method 'stop' on bean with name 'embeddedMongoServer' threw an
+    // exception: java.lang.IllegalStateException: Couldn't kill mongod process!..."
+    // That seems harmless as the MongoD process shuts down and frees up the port.
+    // There are multiple related issues logged on GitHub:
+    // https://github.com/flapdoodle-oss/de.flapdoodle.embed.mongo/issues?q=is%3Aissue+Couldn%27t+kill+mongod+process%21
+
+    public static final int DFLT_PORT_NUMBER = 27014;
+    public static final String DFLT_REPLICASET_NAME = "rs0";
+    public static final int DFLT_STOP_TIMEOUT_MILLIS = 200;
+
+    private Version.Main mFeatureAwareVersion = Version.Main.V4_0;
+    private int mPortNumber = DFLT_PORT_NUMBER;
+    private String mReplicaSetName = DFLT_REPLICASET_NAME;
+    private long mStopTimeoutMillis = DFLT_STOP_TIMEOUT_MILLIS;
+
+    @Bean
+    public MongodConfig mongodConfig() throws UnknownHostException, IOException {
+
+        Map<String, String> args = new HashMap<>();
+        args.put("--replSet", mReplicaSetName);
+
+        MongodConfig mongodConfig = MongodConfig.builder().version(mFeatureAwareVersion)
+                .args(args)
+                .stopTimeoutInMillis(mStopTimeoutMillis)
+                .cmdOptions(MongoCmdOptions.builder().useNoJournal(false).build())
+                .net(new Net(mPortNumber, Network.localhostIsIPv6())).build();
+        return mongodConfig;
+    }
+
+    /**
+     * Initializes a new replica set.
+     * Based on code from https://github.com/flapdoodle-oss/de.flapdoodle.embed.mongo/issues/257
+     */
+    class EmbeddedMongoReplicaSetInitialization {
+
+        EmbeddedMongoReplicaSetInitialization() throws Exception {
+            MongoClient mongoClient = null;
+            try {
+                final BasicDBList members = new BasicDBList();
+                members.add(new Document("_id", 0).append("host", "localhost:" + mPortNumber));
+
+                final Document replSetConfig = new Document("_id", mReplicaSetName);
+                replSetConfig.put("members", members);
+
+                //String connectionUrl = "mongodb://localhost:" + mPortNumber + "/?replicaSet="+mReplicaSetName;
+                String connectionUrl = "mongodb://localhost:" + mPortNumber;
+                mongoClient = MongoClients.create(connectionUrl);
+                final MongoDatabase adminDatabase = mongoClient.getDatabase("admin");
+                adminDatabase.runCommand(new Document("replSetInitiate", replSetConfig));
+
+            } finally {
+                if (mongoClient != null) {
+                    //mongoClient.close();
+                }
+            }
+        }
+    }
+
+    @Bean
+    EmbeddedMongoReplicaSetInitialization embeddedMongoReplicaSetInitialization() throws Exception {
+        return new EmbeddedMongoReplicaSetInitialization();
+    }
+
+    /**
+     * Additional configuration to ensure that the replica set initialization happens after the
+     * {@link MongodExecutable} bean is created. That's it - after the database is started.
+     */
+    @ConditionalOnClass({MongoClient.class, MongodStarter.class})
+    protected static class DependenciesConfiguration
+            extends AbstractDependsOnBeanFactoryPostProcessor {
+
+        DependenciesConfiguration() {
+            super(EmbeddedMongoReplicaSetInitialization.class, null, MongodExecutable.class);
+        }
+    }
+
+}
--- src/test/java/com/shareknowledge/demo/service/StudentServiceTest.java
+++ src/test/java/com/shareknowledge/demo/service/StudentServiceTest.java
@@ -2,15 +2,27 @@ package com.shareknowledge.demo.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.shareknowledge.demo.config.EmbeddedMongoWithTransactionsConfig;
 import com.shareknowledge.demo.dto.StudentModel;
 import com.shareknowledge.demo.repository.StudentRepository;
+import de.flapdoodle.embed.mongo.MongodExecutable;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.event.annotation.AfterTestClass;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StudentServiceTest {
@@ -24,9 +36,23 @@ public class StudentServiceTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private MongodExecutable mongodExecutable;
+
+    @Autowired
+    private MongoClient mongoClient;
+
+    @AfterTestClass
+    void clean() {
+        if (mongodExecutable != null)
+            mongodExecutable.stop();
+    }
+
     @Test
     @Order(1)
     void saveStudentTest() throws JsonProcessingException {
+        MongoDatabase db = mongoClient.getDatabase("demo");
+        db.createCollection("students");
         Long id = saveStudent();
         assert (studentRepository.findAll().size() == 1);
     }
